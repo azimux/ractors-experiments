dccf42a2c930519fcfe68b323c27b770808bccbf
Miles Georgi <azimux@gmail.com>
2026-04-27 20:49:51 -0700

    Lots more logging and experimenting with Fiber

--- scripts/fibers_test.rb
+++ scripts/fibers_test.rb
@@ -9,8 +9,13 @@ $log = Queue.new
 
 $log << "main fiber is #{Fiber.current}"
 
+puts "Main thread #{Thread.current}"
+
 at_exit do
+  puts "exiting"
+  sleep 5
   puts $log.pop until $log.empty?
+  puts "done printing log"
 end
 
 # require "pry"
@@ -27,13 +32,14 @@ class MyFiberScheduler
     ready_fibers = @ready_fibers = Queue.new
 
     $log << "scheduler created in #{Fiber.current}"
-
     main_fiber = Fiber.current
 
-    @idle_fiber = Fiber.new(blocking: true) do
+    @idle_fiber = Fiber.new(blocking: false) do
       $log << "scheduler fiber is #{Fiber.current}"
+      puts "Scheduler thread #{Thread.current}"
 
       loop do
+        $log << "ready fiber count #{@ready_fibers.size}"
         now = Time.now
 
         remove = []
@@ -46,25 +52,56 @@ class MyFiberScheduler
           end
         end
 
-        remove.each do |pair|
-          wake_at.delete(pair)
+        unless remove.empty?
+          remove.each do |pair|
+            wake_at.delete(pair)
+          end
+          next
+        end
+
+        if @ready_fibers.empty? && @wake_at.empty? && @closing
+          puts "breaking"
+          $log << "breaking"
+          break
         end
 
         # puts "idle fiber yielding back"
         if @ready_fibers.empty?
           @running = false
-          sleep 0.1
+
+          if @closing
+            # just transfer to all remaining waiting fibers
+            until wake_at.empty?
+              @wake_at = wake_at.sort_by(&:first)
+              @wake_at.shift => Time, fiber
+              $log << "transferring to #{fiber} at close"
+              fiber.transfer
+            end
+
+            $log << "sleeping to wait for stuff to wake up to close"
+            # sleep 0.1
+          elsif @wait_until_all_done
+            $log << "sleeping to wait for stuff to wake up"
+            sleep 0.1
+          else
+            $log << "Transferring to main #{main_fiber}"
+            main_fiber.transfer
+          end
         else
           @running = true
           fiber = @ready_fibers.pop
 
-          $log << "Transferring to #{fiber}"
-          fiber.transfer
+          if fiber.alive?
+            $log << "Transferring to #{fiber}"
+            fiber.transfer
+            $log << "control returned from #{fiber}?"
+          else
+            $log << "Whoa, #{fiber} is dead"
+          end
         end
       end
     end
 
-    ready_fibers << Fiber.current
     @idle_fiber.transfer
   end
 
@@ -75,6 +112,8 @@ class MyFiberScheduler
     raise if timeout
 
     $log << "block called #{Fiber.current}"
+
+    @idle_fiber.transfer
 
     true
   end
@@ -97,6 +136,29 @@ class MyFiberScheduler
     run_some_other_fiber unless running?
   end
 
+  def close
+    # if !@ready_fibers.empty? || !@wake_at.empty?
+    #   $log << "wtf!!"
+    #   $log << caller_locations
+    # end
+
+    $log << "closing!!"
+    @closing = true
+    @idle_fiber.transfer if @idle_fiber.alive?
+  end
+
+  def wait_until_all_fibers_done
+    @wait_until_all_done = true
+    $log << "transferring control to the scheduler from #{Fiber.current}"
+    @idle_fiber.transfer until nothing_to_do?
+  ensure
+    @wait_until_all_done = false
+  end
+
+  def nothing_to_do?
+    !@idle_fiber.alive? || (@ready_fibers.empty? && @wake_at.empty?)
+  end
+
   def kernel_sleep(duration = nil)
     raise if Fiber.current == @idle_fiber
 
@@ -115,10 +177,10 @@ class MyFiberScheduler
 
   def fiber(&)
     fiber = Fiber.new(blocking: false, &)
+
     $log << "created #{fiber} from #{Fiber.current}"
 
-    @ready_fibers << Fiber.current
-    fiber.transfer
+    @ready_fibers << fiber
 
     fiber
   end
@@ -135,33 +197,48 @@ Fiber.set_scheduler(scheduler)
 #   100_000.times { puts it }
 # end
 
-sleep 1
-
 f1 = Fiber.schedule do
+  puts "f1 thread is #{Thread.current}"
   $log << "f1 is #{Fiber.current}"
+  $log << "printing 1.1"
   puts 1.1
   sleep 1
+  $log << "printing 1.2"
   puts 1.2
+  $log << "printing 1.3"
   puts 1.3
 end
 
 f2 = Fiber.schedule do
   $log << "f2 is #{Fiber.current}"
+  $log << "printing 2.1"
   puts 2.1
+  $log << "printing 2.2"
   puts 2.2
+  $log << "printing 2.3"
   puts 2.3
 end
 
 f3 = Fiber.schedule do
   $log << "f3 is #{Fiber.current}"
+  $log << "printing 3.1"
   puts 3.1
+  $log << "printing 3.2"
   puts 3.2
+  $log << "printing 3.3"
   puts 3.3
 end
 
-sleep 5
+$log << "main 1"
+puts "main 1"
 
 # [f1, f2, f3].each(&:resume)
+sleep 5
+scheduler.close
+puts "main 2"
+$log << "main 2"
+sleep 5
+$log << "main 3"
 
 # sleep 2
 #
